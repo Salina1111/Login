dc3e75f0ae4956d679affeb48dc542233b0c1dd6
Salina <salina.sallu@outlook.com>
2018-11-20 23:47:56 +0545

    register and login is done

---
+++ routes/route.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var router = express.Router();
+var register= require("../modules/controller/register");
+var login = require('../modules/controller/login');
+
+
+router.post('/post',register.registerUser);
+router.post('/get', login.loginUser);
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,28 @@
+//Import Modules
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+const mongoose = require("mongoose");
+const route = require("./routes/route");
+const config = require("./modules/config/config");
+
+//Connect to mongoDb
+mongoose.connect(config.database);
+
+mongoose.connection
+  .once("open", function() {
+    console.log("Connection has been made, now make fireworks!");
+  })
+  .on("error", function(error) {
+    console.log("Connection error:", error);
+  });
+
+app.use(parser.json());
+app.use("/api/", route);
+
+// const hostname = "127.0.0.1";
+// const port = 3000;
+
+app.listen(3000, 'localhost', () => {
+  console.log(`Server is running`);
+});
